68c3dd2d412f609acaa53c0b65e247edd1a6557f
Valdis Guntis Bukalders <vgbukalders@gmail.com>
2023-02-27 10:46:33 +0200

    extract constants

--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,20 @@ struct ImgInfo {
     points: Vec<Point>,
 }
 
-const PITCH_START: f64 = 0.2;
-const PITCH_INCREMENT: f64 = 0.01;
-const YAW_START: f64 = 0.0;
-const YAW_INCREMENT: f64 = 0.013;
+const ANIMATION_DURATION: f32 = 6.0;
+const FPS: f32 = 30.0;
+const FRAME_DELAY: u32 = (1.0 / FPS * 1000.0) as u32;
+const FRAME_AMOUNT: u16 = ANIMATION_DURATION as u16 * FPS as u16;
+const PITCH_START: f64 = 0.0;
+const PITCH_END: f64 = 0.7;
+const PITCH_INCREMENT: f64 = (PITCH_END - PITCH_START) / FRAME_AMOUNT as f64;
+const YAW_START: f64 = 0.2;
+const YAW_END: f64 = 5.0;
+const YAW_INCREMENT: f64 = (YAW_END - YAW_START) / FRAME_AMOUNT as f64;
 const SCALE: f64 = 0.8;
 const IMG_WIDTH: u32 = 1280;
 const IMG_HEIGHT: u32 = 720;
-const FRAME_DELAY: u32 = 100;
 const POINT_SIZE: f32 = 0.7;
-const FRAME_AMOUNT: u8 = 60;
 
 fn process_image(input_path: &str) -> Option<ImgInfo> {
     let img = image::open(&Path::new(input_path));
@@ -39,21 +43,23 @@ fn process_image(input_path: &str) -> Option<ImgInfo> {
     }
 
     let mut points: Vec<Point> = Vec::new();
+    let width = img.as_ref().unwrap().width();
+    let height = img.as_ref().unwrap().height();
 
     for p in img.as_ref().unwrap().pixels() {
         // print rgb value of pixel
         points.push(Point {
             x: p.0,
-            y: p.1,
+            y: height - p.1,
             r: p.2[0],
             g: p.2[1],
             b: p.2[2],
         });
     }
     return Some(ImgInfo {
-        points: points,
-        width: img.as_ref().unwrap().width(),
-        height: img.as_ref().unwrap().height(),
+        points,
+        width,
+        height,
     });
 }
 
@@ -71,7 +77,12 @@ fn main() {
     if let Some('\r') = s.chars().next_back() {
         s.pop();
     }
-    let img_info = process_image(&s).unwrap();
+    let img_info_result = process_image(&s);
+    if img_info_result.is_none() {
+        println!("Could not process image {}", s);
+        return;
+    }
+    let img_info = img_info_result.unwrap();
 
     fs::create_dir_all("images").unwrap();
     let start = Instant::now();
@@ -79,7 +90,6 @@ fn main() {
     let red_root = BitMapBackend::gif("images/r-val.gif", (IMG_WIDTH, IMG_HEIGHT), FRAME_DELAY)
         .unwrap()
         .into_drawing_area();
-    red_root.fill(&WHITE).unwrap();
     let mut red_chart = ChartBuilder::on(&red_root)
         .margin(20)
         .caption(format!("Red values of {s}"), ("sans-serif", 40))
@@ -89,7 +99,6 @@ fn main() {
     let blue_root = BitMapBackend::gif("images/b-val.gif", (IMG_WIDTH, IMG_HEIGHT), FRAME_DELAY)
         .unwrap()
         .into_drawing_area();
-    blue_root.fill(&WHITE).unwrap();
     let mut blue_chart = ChartBuilder::on(&blue_root)
         .margin(20)
         .caption(format!("Blue values of {s}"), ("sans-serif", 40))
@@ -99,14 +108,13 @@ fn main() {
     let green_root = BitMapBackend::gif("images/g-val.gif", (IMG_WIDTH, IMG_HEIGHT), FRAME_DELAY)
         .unwrap()
         .into_drawing_area();
-    green_root.fill(&WHITE).unwrap();
     let mut green_chart = ChartBuilder::on(&green_root)
         .margin(20)
         .caption(format!("Green values of {s}"), ("sans-serif", 40))
         .build_cartesian_3d(0..img_info.width, 0..img_info.height, 0..255 as u32)
         .unwrap();
 
-    let mut generate_red_frame = |i: &u8| {
+    let mut generate_red_frame = |i: &u16| {
         // Generate red frame
         red_root.fill(&WHITE).unwrap();
         red_chart.with_projection(|mut pb| {
@@ -132,7 +140,7 @@ fn main() {
         red_root.present().unwrap();
     };
 
-    let mut generate_green_frame = |i: &u8| {
+    let mut generate_green_frame = |i: &u16| {
         green_root.fill(&WHITE).unwrap();
         green_chart.with_projection(|mut pb| {
             pb.pitch = PITCH_START + PITCH_INCREMENT * *i as f64;
@@ -157,7 +165,7 @@ fn main() {
         green_root.present().unwrap();
     };
 
-    let mut generate_blue_frame = |i: &u8| {
+    let mut generate_blue_frame = |i: &u16| {
         // Generate blue frame
         blue_root.fill(&WHITE).unwrap();
         blue_chart.with_projection(|mut pb| {
@@ -182,6 +190,8 @@ fn main() {
         blue_chart.configure_axes().draw().unwrap();
         blue_root.present().unwrap();
     };
+
+    println!("Generating total of {} frames", FRAME_AMOUNT);
 
     for i in 0..FRAME_AMOUNT {
         let start_frame = Instant::now();
